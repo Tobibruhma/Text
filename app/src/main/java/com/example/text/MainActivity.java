186f6a01ce835ef0ea0aad70385481d4e8da276d
Inkasator1337 <sbenation@mail.ru>
2021-04-15 14:14:58 +0500

    Initial commit

---
+++ app/src/main/java/com/example/text/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinButton , loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinButton = {Button}findViewById(R.id.buttonreg);
+        loginButton = {Button}findViewById(R.id.buttonsign);
+
+        loginButton
+    }
+}
